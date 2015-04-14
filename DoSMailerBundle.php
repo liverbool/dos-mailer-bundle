178458492fe625a1013f655efb2aef2cdbb0522a
ツ Liverbool <nukboon@gmail.com>
2015-04-14 21:56:10 +0700

    dependency bundle

--- DoSMailerBundle.php
+++ DoSMailerBundle.php
@@ -6,4 +6,13 @@ use DoS\ResourceBundle\DependencyInjection\AbstractResourceBundle;
 
 class DoSMailerBundle extends AbstractResourceBundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    protected function getDependencyBundles()
+    {
+        return array(
+            new \Sylius\Bundle\MailerBundle\SyliusMailerBundle(),
+        );
+    }
 }
