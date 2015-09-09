28882d5b4cb563b98b2e531921a1d152d6fd25b8
ツ Liverbool <nukboon@gmail.com>
2015-09-09 21:25:18 +0700

    add maligun transporter.

--- DependencyInjection/DoSMailerExtension.php
+++ DependencyInjection/DoSMailerExtension.php
@@ -4,7 +4,9 @@ namespace DoS\MailerBundle\DependencyInjection;
 
 use DoS\ResourceBundle\DependencyInjection\AbstractResourceExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\DefinitionDecorator;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Reference;
 
 class DoSMailerExtension extends AbstractResourceExtension implements PrependExtensionInterface
 {
@@ -20,6 +22,8 @@ class DoSMailerExtension extends AbstractResourceExtension implements PrependExt
             self::CONFIGURE_VALIDATORS |
             self::CONFIGURE_FORMS
         );
+
+        $this->setupMailgun($container);
     }
 
     /**
@@ -30,8 +34,25 @@ class DoSMailerExtension extends AbstractResourceExtension implements PrependExt
         $configs = $container->getExtensionConfig($this->getAlias());
         // use the Configuration class to generate a config array with
         $config = $this->processConfiguration(new Configuration(), $configs);
+
         // no sylius config
         unset($config['object_manager']);
         $container->prependExtensionConfig('sylius_mailer', $config);
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     */
+    private function setupMailgun(ContainerBuilder $container)
+    {
+        $definitionDecorator = new DefinitionDecorator('swiftmailer.transport.eventdispatcher.abstract');
+        $container->setDefinition('mailgun.swift_transport.eventdispatcher', $definitionDecorator);
+        $container->getDefinition('mailgun.swift_transport.transport')
+            ->replaceArgument(0, new Reference('mailgun.swift_transport.eventdispatcher'))
+        ;
+
+        //set some alias
+        $container->setAlias('mailgun', 'mailgun.swift_transport.transport');
+        $container->setAlias('swiftmailer.mailer.transport.mailgun', 'mailgun.swift_transport.transport');
     }
 }
---
+++ Transportor/MailgunTransport.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace DoS\MailerBundle\Transportor;
+
+use Mailgun\Mailgun;
+use Swift_Events_EventListener;
+use Swift_Events_SendEvent;
+use Swift_Mime_Message;
+use Swift_Transport;
+
+class MailgunTransport implements Swift_Transport
+{
+    /**
+     * @var \Mailgun\Mailgun mailgun
+     */
+    private $mailgun;
+
+    /**
+     * @var string domain
+     */
+    private $domain;
+
+    /**
+     * The event dispatcher from the plugin API.
+     *
+     * @var \Swift_Events_EventDispatcher eventDispatcher
+     */
+    private $eventDispatcher;
+
+    /**
+     * @param \Swift_Events_EventDispatcher $eventDispatcher
+     * @param Mailgun $mailgun
+     * @param $domain
+     */
+    public function __construct(\Swift_Events_EventDispatcher $eventDispatcher, Mailgun $mailgun, $domain)
+    {
+        $this->eventDispatcher = $eventDispatcher;
+        $this->domain = $domain;
+        $this->mailgun = $mailgun;
+    }
+
+    /**
+     * Not used.
+     */
+    public function isStarted()
+    {
+        return true;
+    }
+
+    /**
+     * Not used.
+     */
+    public function start()
+    {
+    }
+
+    /**
+     * Not used.
+     */
+    public function stop()
+    {
+    }
+
+    /**
+     * Send the given Message.
+     *
+     * Recipient/sender data will be retrieved from the Message API.
+     * The return value is the number of recipients who were accepted for delivery.
+     *
+     * @param Swift_Mime_Message $message
+     * @param string[] $failedRecipients An array of failures by-reference
+     *
+     * @return integer number of mails sent
+     * @throws \Swift_TransportException
+     */
+    public function send(Swift_Mime_Message $message, &$failedRecipients = null)
+    {
+        if ($evt = $this->eventDispatcher->createSendEvent($this, $message)) {
+            $this->eventDispatcher->dispatchEvent($evt, 'beforeSendPerformed');
+
+            if ($evt->bubbleCancelled()) {
+                return 0;
+            }
+        }
+
+        if (null === $message->getHeaders()->get('To')) {
+            throw new \Swift_TransportException(
+                'Cannot send message without a recipient'
+            );
+        }
+
+        $postData = $this->prepareRecipients($message);
+        $result = $this->mailgun->sendMessage($this->domain, $postData, $message->toString());
+
+        if ($evt) {
+            $evt->setResult(
+                $result->http_response_code == 200 ? Swift_Events_SendEvent::RESULT_SUCCESS : Swift_Events_SendEvent::RESULT_FAILED
+            );
+            $this->eventDispatcher->dispatchEvent($evt, 'sendPerformed');
+        }
+
+        return 1;
+    }
+
+    /**
+     * Register a plugin in the Transport.
+     *
+     * @param Swift_Events_EventListener $plugin
+     */
+    public function registerPlugin(Swift_Events_EventListener $plugin)
+    {
+        $this->eventDispatcher->bindEventListener($plugin);
+    }
+
+    /**
+     * @param Swift_Mime_Message $message
+     *
+     * @return array
+     */
+    protected function prepareRecipients(Swift_Mime_Message $message)
+    {
+        $headerNames = array('from', 'to', 'bcc', 'cc');
+        $messageHeaders = $message->getHeaders();
+        $postData = array();
+
+        foreach ($headerNames as $name) {
+            /** @var \Swift_Mime_Headers_MailboxHeader $h */
+            $h = $messageHeaders->get($name);
+            $postData[$name] = $h === null ? array() : $h->getAddresses();
+        }
+        // Merge 'bcc' and 'cc' into 'to'.
+        $postData['to'] = array_merge($postData['to'], $postData['bcc'], $postData['cc']);
+
+        unset($postData['bcc']);
+        unset($postData['cc']);
+
+        // Remove Bcc to make sure it is hidden
+        $messageHeaders->removeAll('bcc');
+
+        return $postData;
+    }
+}
