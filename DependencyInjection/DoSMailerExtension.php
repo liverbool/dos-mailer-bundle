5ca94cfde32e6fea970c3880a3b6d2ba28427736
ツ Liverbool <nukboon@gmail.com>
2015-09-09 21:30:59 +0700

    fix service name.

--- DependencyInjection/DoSMailerExtension.php
+++ DependencyInjection/DoSMailerExtension.php
@@ -46,13 +46,13 @@ class DoSMailerExtension extends AbstractResourceExtension implements PrependExt
     private function setupMailgun(ContainerBuilder $container)
     {
         $definitionDecorator = new DefinitionDecorator('swiftmailer.transport.eventdispatcher.abstract');
-        $container->setDefinition('mailgun.swift_transport.eventdispatcher', $definitionDecorator);
-        $container->getDefinition('mailgun.swift_transport.transport')
-            ->replaceArgument(0, new Reference('mailgun.swift_transport.eventdispatcher'))
+        $container->setDefinition('dos.mailer.transportor.mailgun.eventdispatcher', $definitionDecorator);
+        $container->getDefinition('dos.mailer.transportor.mailgun')
+            ->replaceArgument(0, new Reference('dos.mailer.transportor.mailgun.eventdispatcher'))
         ;
 
         //set some alias
-        $container->setAlias('mailgun', 'mailgun.swift_transport.transport');
-        $container->setAlias('swiftmailer.mailer.transport.mailgun', 'mailgun.swift_transport.transport');
+        $container->setAlias('mailgun', 'dos.mailer.transportor.mailgun');
+        $container->setAlias('swiftmailer.mailer.transport.mailgun', 'dos.mailer.transportor.mailgun');
     }
 }
