7e38795652f6bf98da520ccba704f0abedb6ddbf
ツ Liverbool <nukboon@gmail.com>
2015-11-23 20:46:35 +0700

    bugfix

--- Renderer/Adapter/TwigMarkdownAdapter.php
+++ Renderer/Adapter/TwigMarkdownAdapter.php
@@ -4,7 +4,10 @@ namespace DoS\MailerBundle\Renderer\Adapter;
 
 use Knp\Bundle\MarkdownBundle\Helper\MarkdownHelper;
 use Sylius\Bundle\MailerBundle\Renderer\Adapter\TwigAdapter;
+use Sylius\Component\Mailer\Event\EmailRenderEvent;
 use Sylius\Component\Mailer\Model\EmailInterface;
+use Sylius\Component\Mailer\Renderer\RenderedEmail;
+use Sylius\Component\Mailer\SyliusMailerEvents;
 
 /** @deprecated Use SyliusMailerEvents::EMAIL_PRE_RENDER event */
 class TwigMarkdownAdapter extends TwigAdapter
@@ -35,6 +38,31 @@ class TwigMarkdownAdapter extends TwigAdapter
             $mail->setContent($this->markdownHelper->transform($mail->getContent()));
         }
 
-        return parent::render($mail, $data);
+        if (null !== $email->getTemplate()) {
+            $data = $this->twig->mergeGlobals($data);
+
+            /** @var \Twig_Template $template */
+            $template = $this->twig->loadTemplate($email->getTemplate());
+
+            $subject = $template->renderBlock('subject', $data);
+            $body = $template->renderBlock('body', $data);
+        } else {
+            // Need evironment to parse twig functions, filters, etc ...
+            $twig = $this->twig; //new \Twig_Environment(new \Twig_Loader_Array(array()));
+
+            $subjectTemplate = $twig->createTemplate($email->getSubject());
+            $bodyTemplate = $twig->createTemplate($email->getContent());
+
+            $subject = $subjectTemplate->render($data);
+            $body = $bodyTemplate->render($data);
+        }
+
+        /** @var EmailRenderEvent $event */
+        $event = $this->dispatcher->dispatch(
+            SyliusMailerEvents::EMAIL_PRE_RENDER,
+            new EmailRenderEvent(new RenderedEmail($subject, $body))
+        );
+
+        return $event->getRenderedEmail();
     }
 }
